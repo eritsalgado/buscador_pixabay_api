d587d87b5f9c321d59727a98c47e1e3a06d0b02e
eritsalgado <eritsg-moon@hotmail.com>
2020-08-08 16:02:34 -0500

    Espaciado en margen Y

--- src/components/Imagen.js
+++ src/components/Imagen.js
@@ -11,7 +11,7 @@ const Imagen = ({imagen}) => {
         views
     } = imagen
     return (
-        <div className="col-12 col-sm-6 col-md-4 col-lg-3">
+        <div className="col-12 col-sm-6 col-md-4 col-lg-3 my-3">
             <div className="card">
                 <img 
                     src={previewURL} 
